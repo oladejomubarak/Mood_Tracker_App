538134c4bb6aca4877ea6e07752ea3ebf47a91c0
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-18 13:53:30 +0100

    commiting....

--- src/main/java/africa/irespond/moodtracker/model/AnnualMoodTracker.java
+++ src/main/java/africa/irespond/moodtracker/model/AnnualMoodTracker.java
@@ -1,4 +1,20 @@
 package africa.irespond.moodtracker.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class AnnualMoodTracker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String todayDate;
+    private Mood mood;
+    private SocialMoodInfluence socialMoodInfluence;
+    private WeatherMoodInfluence weatherMoodInfluence;
+    @Column(length = 5000)
+    private String comment;
+    private double ratings;
+
 }
